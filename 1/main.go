818575280160d55baa03406b55e64299aebc827a
Mark Quinn <mark.quinn@infinityworks.com>
2018-12-11 12:46:04 +0000

    Day 1 - Part 1

---
+++ 1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var t int64 = 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		t += i
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(t)
+}
